e9f3330b8508a213cc53f5910da61fb99f0b9044
Bhagyashree Nath <bhagyashreenathcse@gmail.com>
2025-12-15 12:30:22 +0530

    ui-up

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -87,3 +87,29 @@ exports.deleteProduct = catchAsyncError(async (req, res, next) => {
     messaage: "Product removed successfully",
   });
 });
+
+// Create New Review or Update the review
+exports.createProductReview = catchAsyncError(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+  if (isReviewed) {
+    product.reviews.forEach((rev) =>{
+      if (rev.user.toString() === req.user._id.toString())
+      (rev.rating = rating), (rev.comment = comment);
+    });
+  } else {
+    product.reviews.push(review);
+  }
+});
